c8756a093fcd6ee10cce5ed40d16f4d1f4beacb3
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-05 20:22:30 -0500

    Initial Commit

---
+++ SpeechToTextDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechToTextDemo
+//
+//  Created by Kritima Kukreja on 2020-03-05.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
